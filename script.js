e83a98ad32c18786b9afb909ad382c89eab96e94
benTKam <56923669+benTKam@users.noreply.github.com>
2023-04-27 19:10:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-// Set up the SVG container
 var margin = { top: 50, right: 50, bottom: 50, left: 50 },
-  width = 1200 - margin.left - margin.right,
+  width = 1800 - margin.left - margin.right,
   height = 800 - margin.top - margin.bottom;
 
 var svg = d3
@@ -12,7 +11,7 @@ var svg = d3
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 // Load the data from the TSV file
-d3.tsv("merged_movies.tsv")
+d3.tsv("20006.tsv")
   .then(function(data) {
     console.log(data); // Verify that the data is being loaded correctly
     
@@ -28,10 +27,10 @@ d3.tsv("merged_movies.tsv")
       .range([height, 0]);
 
     // Define color scale
-    var colorScale = d3.scaleOrdinal(d3.schemeCategory10)
-      .domain(d3.group(data, function(d) {
-        return d.genres.split(",")[0];
-      }).keys());
+    const distinctValues = [...new Set(data.map(d => d.genres.split(",")[0]))];
+    var colorScale = d3.scaleOrdinal().domain(distinctValues)              
+      .range(d3.schemeSet2);
+   
 
     // Add the x and y axes
     svg
@@ -53,8 +52,52 @@ d3.tsv("merged_movies.tsv")
       .attr("cx", function(d) { return xScale(d.startYear); })
       .attr("cy", function(d) { return yScale(d.averageRating); })
       .attr("r", 5)
-      .attr("fill", function(d) { return colorScale(d.genres.split(",")[0]); });
+      .attr("fill", function(d) { return colorScale(d.genres.split(",")[0]); })
+      scatterPoints.on('mouseover', (event, d) => {
+        d3.select(event.currentTarget).style("stroke", "black");
+        d3.select('#tooltip')
+          .style('display', 'block')
+          .html('<h1 class="tooltip-header">' + "Title: " + d.primaryTitle + " Genre: " + d.genres + '</h1>');
+        })
+        .on('mouseleave', (event) => {  //when mouse isn’t over point
+          d3.select('#tooltip').style('display', 'none'); // hide tooltip
+          d3.select(event.currentTarget) //remove the stroke from point
+              .style("stroke", "none");
+
+
+      });
+      // Add a legend for each genre color represented
+var legend = d3.select('#legend') // Position the legend in the top right corner
+
+var legendRectSize = 18; // The size of each legend rectangle
+var legendSpacing = 4; // The spacing between each legend rectangle
+
+var legendItems = legend.selectAll(".legend-item")
+.data(distinctValues)
+.enter()
+.append("g")
+.attr("class", "legend-item")
+.attr("transform", function(d, i) {
+  var height = legendRectSize + legendSpacing;
+  var offset = height * distinctValues.length / 2;
+  var x = 0;
+  var y = i * height - offset;
+  return "translate(" + x + "," + y + ")";
+});
+
+legendItems.append("rect")
+.attr("width", legendRectSize)
+.attr("height", legendRectSize)
+.style("fill", colorScale);
+
+legendItems.append("text")
+.attr("x", legendRectSize + legendSpacing)
+.attr("y", legendRectSize - legendSpacing)
+.text(function(d) { return d; });
   })
   .catch(function(error) {
     console.log(error);
   });
+
+
+// Create the legend
