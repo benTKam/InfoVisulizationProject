8f7994cb7be0c296665f7605112e78ec8b3837f6
krishnaapatell <111795949+krishnaapatell@users.noreply.github.com>
2023-05-02 18:18:24 -0400

    changes made

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var margin = { top: 50, right: 50, bottom: 50, left: 50 },
   width = 1800 - margin.left - margin.right,
-  height = 800 - margin.top - margin.bottom;
+  height = 1500 - margin.top - margin.bottom;
 
 var svg = d3
   .select("#graph-container")
@@ -10,15 +10,46 @@ var svg = d3
   .append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+  // Define the steps of the scroll story
+var steps = [
+  {
+    title: "Step 1",
+    description: "This is the description of step 1.",
+    startYear: 2000,
+    endYear: 2005
+  },
+  {
+    title: "Step 2",
+    description: "This is the description of step 2.",
+    startYear: 2005,
+    endYear: 2010
+  },
+  {
+    title: "Step 3",
+    description: "This is the description of step 3.",
+    startYear: 2010,
+    endYear: 2015
+  },
+  {
+    title: "Step 4",
+    description: "This is the description of step 4.",
+    startYear: 2015,
+    endYear: 2020
+  },
+];
+
+// Define the current step
+var currentStep = 0;
+
 // Load the data from the TSV file
 d3.tsv("20006.tsv")
-  .then(function(data) {
+  .then(function (data) {
     console.log(data); // Verify that the data is being loaded correctly
-    
+
     // Define scales for the x and y axes
     var xScale = d3
       .scaleLinear()
-      .domain(d3.extent(data, function(d) { return d.startYear; }))
+      .domain(d3.extent(data, function (d) { return d.startYear; }))
       .range([0, width]);
 
     var yScale = d3
@@ -28,9 +59,8 @@ d3.tsv("20006.tsv")
 
     // Define color scale
     const distinctValues = [...new Set(data.map(d => d.genres.split(",")[0]))];
-    var colorScale = d3.scaleOrdinal().domain(distinctValues)              
+    var colorScale = d3.scaleOrdinal().domain(distinctValues)
       .range(d3.schemeSet2);
-   
 
     // Add the x and y axes
     svg
@@ -49,55 +79,45 @@ d3.tsv("20006.tsv")
       .enter()
       .append("circle")
       .attr("class", "scatter-point")
-      .attr("cx", function(d) { return xScale(d.startYear); })
-      .attr("cy", function(d) { return yScale(d.averageRating); })
+      .attr("cx", function (d) { return xScale(d.startYear); })
+      .attr("cy", function (d) { return yScale(d.averageRating); })
       .attr("r", 5)
-      .attr("fill", function(d) { return colorScale(d.genres.split(",")[0]); })
-      scatterPoints.on('mouseover', (event, d) => {
-        d3.select(event.currentTarget).style("stroke", "black");
-        d3.select('#tooltip')
-          .style('display', 'block')
-          .html('<h1 class="tooltip-header">' + "Title: " + d.primaryTitle + " Genre: " + d.genres + '</h1>');
-        })
-        .on('mouseleave', (event) => {  //when mouse isn’t over point
-          d3.select('#tooltip').style('display', 'none'); // hide tooltip
-          d3.select(event.currentTarget) //remove the stroke from point
-              .style("stroke", "none");
+      .attr("fill", function (d) { return colorScale(d.genres.split(",")[0]); })
+    scatterPoints.on('mouseover', (event, d) => {
+      d3.select(event.currentTarget).style("stroke", "black");
+      d3.select('#tooltip')
+        .style('display', 'block')
+        .html('<h1 class="tooltip-header">' + "Title: " + d.primaryTitle + " Genre: " + d.genres + '</h1>');
+    })
+      .on('mouseleave', (event) => {  //when mouse isn’t over point
+        d3.select('#tooltip').style('display', 'none'); // hide tooltip
+        d3.select(event.currentTarget)
+          .style("stroke", "none");
+      })
 
+    // Add legend
+    var legend = svg.selectAll(".legend")
+      .data(distinctValues)
+      .enter().append("g")
+      .attr("class", "legend")
+      .attr("transform", function (d, i) { return "translate(0," + i * 20 + ")"; });
 
+    // Add colored squares to legend
+    legend.append("rect")
+      .attr("x", width - 18)
+      .attr("width", 18)
+      .attr("height", 18)
+      .style("fill", colorScale);
+
+    // Add text to legend
+    legend.append("text")
+      .attr("x", width - 24)
+      .attr("y", 9)
+      .attr("dy", ".35em")
+      .style("text-anchor", "end")
+      .text(function (d) { return d; });
+      })
+
+      .catch(function (error) {
+      console.log(error); // Log any errors
       });
-      // Add a legend for each genre color represented
-var legend = d3.select('#legend') // Position the legend in the top right corner
-
-var legendRectSize = 18; // The size of each legend rectangle
-var legendSpacing = 4; // The spacing between each legend rectangle
-
-var legendItems = legend.selectAll(".legend-item")
-.data(distinctValues)
-.enter()
-.append("g")
-.attr("class", "legend-item")
-.attr("transform", function(d, i) {
-  var height = legendRectSize + legendSpacing;
-  var offset = height * distinctValues.length / 2;
-  var x = 0;
-  var y = i * height - offset;
-  return "translate(" + x + "," + y + ")";
-});
-
-legendItems.append("rect")
-.attr("width", legendRectSize)
-.attr("height", legendRectSize)
-.style("fill", colorScale);
-
-legendItems.append("text")
-.attr("x", legendRectSize + legendSpacing)
-.attr("y", legendRectSize - legendSpacing)
-.text(function(d) { return d; });
-  })
-  .catch(function(error) {
-    console.log(error);
-  });
-
-
-// Create the legend
