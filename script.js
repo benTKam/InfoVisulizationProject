aa871a2b53d031b94683f9c6dfa2605823909c33
MarcoKosasih <marco105.k@gmail.com>
2023-05-03 18:39:01 -0400

    filter the top 100 movies

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
-const customColors = [
-    "#1f77b4",
-    "#aec7e8",
-    "#ff7f0e",
-    "#ffbb78",
-    "#2ca02c",
-    "#98df8a",
-    "#d62728",
-    "#ff9896",
-    "#9467bd",
-    "#c5b0d5",
-    "#8c564b",
-    "#c49c94",
-    "#e377c2",
-    "#f7b6d2",
-    "#7f7f7f",
-    "#c7c7c7",
-    "#bcbd22",
-    "#dbdb8d",
-    "#17becf",
-    "#9edae5"
-  ];
+const customColors = {
+    "Comedy": "#1f77b4",
+    "Action": "#aec7e8",
+    "Biography": "#ff7f0e",
+    "Drama": "#ffbb78",
+    "Adventure": "#2ca02c",
+    "Animation": "#98df8a",
+    "Crime": "#d62728",
+    "Fantasy": "#ff9896",
+    "Documentary": "#9467bd",
+    "Horror": "#c5b0d5",
+    "Thriller": "#8c564b",
+    "Mystery": "#c49c94",
+    "Romance": "#e377c2",
+    "Family": "#f7b6d2",
+    "Sci-Fi": "#7f7f7f",
+    "Music": "#c7c7c7",
+    "History": "#bcbd22"
+};
 
 let margin = { top: 50, right: 50, bottom: 50, left: 100 },
   width = 2000 - margin.left - margin.right,
@@ -72,40 +69,53 @@ d3.select("#filter-button").on("click", function () {
   }
 });
 
-
-
 let selectedGenre = '';
 
 // Add event listener to button
 document.getElementById("sort-by-genre-button").addEventListener("click", function() {
   // Define an array of the distinct genre values
   const distinctGenres = [...new Set(data.map(d => d.genres.split(",")[0]))];
-  const genreColorMap = {};
-  distinctGenres.forEach((genre, index) => {
-    genreColorMap[genre] = customColors[index % customColors.length];
-  });
+  let colorScale = d3.scaleOrdinal()
+  .domain(distinctGenres)
+  .range(Object.values(customColors));
+
   // Get the index of the currently selected genre
   let currentIndex = distinctGenres.indexOf(selectedGenre);
-  
+
   // Increment the index to select the next genre, or reset to 0 if at the end
   currentIndex = (currentIndex + 1) % distinctGenres.length;
-  
+
   // Set the selected genre to the new index
   selectedGenre = distinctGenres[currentIndex];
-  
+
   // Filter the data by the selected genre
   const filteredData = data.filter(d => d.genres.split(",")[0] === selectedGenre)
-  .map(d => {
-    return {
-      ...d,
-      color: genreColorMap[d.genres.split(",")[0]]
-    };
-  });
+    .map(d => {
+      return {
+        ...d,
+        color: colorScale(d.genres.split(".")[0])
+      };
+    });
+    d3.select('#tooltip')
+    .style('display', 'block')
+    .html('<h1 class="tooltip-header">' + " Genre: " + selectedGenre + '</h1>');
   // Call the updateScatterPlot function with the filtered data
-  
   updateScatterPlot(filteredData);
 });
 
+// Add event listener to the "filter-top-100" button
+d3.select("#filter-top-100").on("click", function () {
+  // Sort the data by average rating
+  data.sort(function(a, b) {
+    return b.averageRating - a.averageRating;
+  });
+
+  // Filter the top 100 items
+  let filteredData = data.slice(0, 100);
+
+  // Update the scatter plot with the filtered data
+  updateScatterPlot(filteredData);
+});
 
 // Update the scatter plot with the given data
 function updateScatterPlot(data) {
@@ -120,12 +130,11 @@ function updateScatterPlot(data) {
     .domain([0, 10])
     .range([height, 0]);
 
-  // Creates a custom color scale
-  const distinctGenres = [...new Set(data.map(d => d.genres.split(",")[0]))];
-  const colorScale = d3.scaleOrdinal()
-    .domain(distinctGenres)
-    .range(customColors);
-
+    let distinctGenres = [...new Set(data.map(d => d.genres.split(",")[0]))];
+    let colorScale = d3.scaleOrdinal()
+      .domain(distinctGenres)
+      .range(Object.values(customColors));
+      
   // Remove the existing scatter plot points
   svg.selectAll(".scatter-point").remove();
 
@@ -197,7 +206,8 @@ function updateScatterPlot(data) {
     .attr("y", 9)
     .attr("dy", ".35em")
     .style("text-anchor", "end")
-    .text(d => d);
+    .text(d => d)
+    .style("fill", white);;
 }
 
 
