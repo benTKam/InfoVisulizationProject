f598fbdb2ca151433c1459f9bfc48c20080c3f67
MarcoKosasih <marco105.k@gmail.com>
2023-05-02 20:34:59 -0400

    add both "themeriver" and scatterplot incrementation to code

--- script.js
+++ script.js
@@ -1,143 +1,227 @@
 let margin = { top: 50, right: 50, bottom: 50, left: 50 },
-  width = 1800 - margin.left - margin.right,
+  width = 2000 - margin.left - margin.right,
   height = 1500 - margin.top - margin.bottom;
 
 let svg = d3
-  .select("#section-1")
+  .select("#canvas")
   .append("svg")
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
   .append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-
-// === Scrollytelling boilerplate === //
-function scroll(n, offset, func1, func2) {
-  const el = document.getElementById(n)
-  return new Waypoint({
-    element: document.getElementById(n),
-    handler: function (direction) {
-      direction == 'down' ? func1() : func2();
-    },
-    //start 75% from the top of the div
-    offset: offset
-  });
-};
-
-// Define the steps of the scroll story
-let steps = [
-  {
-    title: "Step 1",
-    description: "This is the description of step 1.",
-    startYear: 2000,
-    endYear: 2005
-  },
-  {
-    title: "Step 2",
-    description: "This is the description of step 2.",
-    startYear: 2005,
-    endYear: 2010
-  },
-  {
-    title: "Step 3",
-    description: "This is the description of step 3.",
-    startYear: 2010,
-    endYear: 2015
-  },
-  {
-    title: "Step 4",
-    description: "This is the description of step 4.",
-    startYear: 2015,
-    endYear: 2020
-  },
-];
-
-// Define the current step
-let currentStep = 0;
+let data;
 
 // Load the data from the TSV file
 d3.tsv("20006.tsv")
-  .then(function (data) {
+  .then(function (loadedData) {
+    data = loadedData;
     console.log(data); // Verify that the data is being loaded correctly
 
-
-
-    // Define scales for the x and y axes
-    let xScale = d3
-      .scaleLinear()
-      .domain(d3.extent(data, function (d) { return d.startYear; }))
-      .range([0, width]);
-
-    let yScale = d3
-      .scaleLinear()
-      .domain([0, 10])
-      .range([height, 0]);
-
-    // Define color scale
-    const distinctValues = [...new Set(data.map(d => d.genres.split(",")[0]))];
-    let colorScale = d3.scaleOrdinal().domain(distinctValues)
-      .range(d3.schemeSet2);
-
-    // Add the x and y axes
-    svg
-      .append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(xScale));
-
-    svg
-      .append("g")
-      .call(d3.axisLeft(yScale));
-
-    // Add a scatter plot point for each movie
-    let data1 = svg
-      .selectAll(".scatter-point")
-      .data(data.filter(function(d){
-        return d.startYear >= 2000 && d.startYear <= 2005;
-      }))
-      .enter()
-      .append("circle")
-      .attr("class", "scatter-point")
-      .attr("cx", function (d) { return xScale(d.startYear); })
-      .attr("cy", function (d) { return yScale(d.averageRating); })
-      .attr("r", 5)
-      .attr("fill", function (d) { return colorScale(d.genres.split(",")[0]); })
-    data1.on('mouseover', (event, d) => {
-      d3.select(event.currentTarget).style("stroke", "black");
-      d3.select('#tooltip')
-        .style('display', 'block')
-        .html('<h1 class="tooltip-header">' + "Title: " + d.primaryTitle + " Genre: " + d.genres + '</h1>');
-    })
-      .on('mouseleave', (event) => {  //when mouse isn’t over point
-        d3.select('#tooltip').style('display', 'none'); // hide tooltip
-        d3.select(event.currentTarget)
-          .style("stroke", "none");
-      })
-
-    // let data2 = 
-
-    // Add legend
-    let legend = svg.selectAll(".legend")
-      .data(distinctValues)
-      .enter().append("g")
-      .attr("class", "legend")
-      .attr("transform", function (d, i) { return "translate(0," + i * 20 + ")"; });
-
-    // Add colored squares to legend
-    legend.append("rect")
-      .attr("x", width - 18)
-      .attr("width", 18)
-      .attr("height", 18)
-      .style("fill", colorScale);
-
-    // Add text to legend
-    legend.append("text")
-      .attr("x", width - 24)
-      .attr("y", 9)
-      .attr("dy", ".35em")
-      .style("text-anchor", "end")
-      .text(function (d) { return d; });
+    // Initialize the scatter plot
+    updateScatterPlot(data);
+    updateThemeRiver(data);
   })
-
   .catch(function (error) {
     console.log(error); // Log any errors
   });
+
+// Add event listener to the filter button
+let currentYear = 1990;
+
+d3.select("#filter-button").on("click", function () {
+  // Filter data based on currentYear
+  let filteredData = data.filter(function (d) {
+    return d.startYear <= currentYear;
+  });
+
+  // Update the scatter plot with the filtered data
+  updateScatterPlot(filteredData);
+
+  // Increment the current year by 1
+  currentYear++;
+  if (currentYear > 2023) {
+    currentYear = 1990;
+  }
+
+
+  // Update the scatter plot with the filtered data
+  updateScatterPlot(filteredData);
+});
+
+
+// Update the scatter plot with the given data
+function updateScatterPlot(data) {
+  // Define scales for the x and y axes
+  let xScale = d3
+    .scaleLinear()
+    .domain(d3.extent(data, function (d) { return d.startYear; }))
+    .range([0, width]);
+
+  let yScale = d3
+    .scaleLinear()
+    .domain([0, 10])
+    .range([height, 0]);
+
+  // Define color scale
+  const distinctValues = [...new Set(data.map(d => d.genres.split(",")[0]))];
+  let colorScale = d3.scaleOrdinal().domain(distinctValues)
+    .range(d3.schemeSet2);
+
+  // Remove the existing scatter plot points
+  svg.selectAll(".scatter-point").remove();
+
+  // Add the new scatter plot points
+  let dataPoints = svg
+    .selectAll(".scatter-point")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("class", "scatter-point")
+    .attr("cx", function (d) { return xScale(d.startYear); })
+    .attr("cy", function (d) { return yScale(d.averageRating); })
+    .attr("r", 10)
+    .attr("fill", function (d) { return colorScale(d.genres.split(",")[0]); });
+
+  // Add tooltip functionality
+  dataPoints.on('mouseover', (event, d) => {
+    d3.select(event.currentTarget).style("stroke", "black");
+    d3.select('#tooltip')
+      .style('display', 'block')
+      .html('<h1 class="tooltip-header">' + "Title: " + d.primaryTitle + " Genre: " + d.genres + '</h1>');
+  })
+    .on('mouseleave', (event) => {  //when mouse isn’t over point
+      d3.select('#tooltip').style('display', 'none'); // hide tooltip
+      d3.select(event.currentTarget)
+        .style("stroke", "none");
+    });
+
+  // Add the x and y axes
+  svg.select(".x-axis").remove();
+  svg
+    .append("g")
+    .attr("class", "x-axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(xScale));
+
+  svg.select(".y-axis").remove();
+  svg
+    .append("g")
+    .attr("class", "y-axis")
+.call(d3.axisLeft(yScale));
+}
+
+
+// Update the theme river graph with the given data
+function updateThemeRiver(data) {
+  // Define the stack layout
+  let stack = d3.stack()
+    .keys([...new Set(data.map(d => d.genres.split(",")[0]))])
+    .value((d, key) => {
+      let movies = genreMap.get(key);
+      return d3.mean(movies, m => m.averageRating);
+    });
+
+  let genreMap = d3.group(data, d => d.genres.split(",")[0]);
+
+
+  // Define scales for the x and y axes
+  let xScale = d3
+    .scaleLinear()
+    .domain(d3.extent(data, function (d) { return d.startYear; }))
+    .range([0, width]);
+
+  let yScale = d3
+    .scaleLinear()
+    .domain([
+      d3.min(stack(data), d => d3.min(d, d => d[0])),
+      d3.max(stack(data), d => d3.max(d, d => d[1]))
+    ])
+    .range([height, 0]);
+
+  // Define color scale
+  const distinctValues = [...new Set(data.map(d => d.genres.split(",")[0]))];
+  let colorScale = d3.scaleOrdinal()
+    .domain(distinctValues)
+    .range(d3.schemeSet2);
+
+  // Remove the existing theme river areas
+  svg.selectAll(".theme-river-area").remove();
+
+  // Add the new theme river areas
+  let area = d3.area()
+    .x((d, i) => xScale(d.data.startYear))
+    .y0(d => yScale(d[0]))
+    .y1(d => yScale(d[1]));
+
+  let themeRiverAreas = svg.selectAll(".theme-river-area")
+    .data(stack(data))
+    .enter()
+    .append("path")
+    .attr("class", "theme-river-area")
+    .attr("d", area)
+    .attr("fill", d => colorScale(d.key))
+    .attr("opacity", 0.8);
+
+  // Add tooltip functionality
+  themeRiverAreas.on('mouseover', (event, d) => {
+    d3.select(event.currentTarget).style("stroke", "black");
+    d3.select('#tooltip')
+      .style('display', 'block')
+      .html('<h1 class="tooltip-header">' + "Genre: " + d.key + '</h1>');
+  })
+    .on('mouseleave', (event) => {  //when mouse isn’t over area
+      d3.select('#tooltip').style('display', 'none'); // hide tooltip
+      d3.select(event.currentTarget)
+    })
+    .on('mousemove', (event, d) => {
+      d3.select('#tooltip')
+        .style('top', (event.pageY - 10) + 'px')
+        .style('left', (event.pageX + 10) + 'px');
+    });
+
+  // Add x-axis
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(xScale).ticks(20))
+    .selectAll("text")
+    .attr("transform", "rotate(-90)")
+    .attr("dx", "-.8em")
+    .attr("dy", ".15em")
+    .style("text-anchor", "end");
+
+  // Add y-axis
+  svg.append("g")
+    .call(d3.axisLeft(yScale))
+    .selectAll("text")
+    .style("text-anchor", "end");
+
+  // Add chart title
+  svg.append("text")
+    .attr("x", (width / 2))
+    .attr("y", 0 - (margin.top / 2))
+    .attr("text-anchor", "middle")
+    .style("font-size", "24px")
+    .text("Average IMDb Ratings of Top 100 Most Popular Movies Each Year by Genre (2006-2016)");
+
+  // Add legend
+  let legend = svg.selectAll(".legend")
+    .data(distinctValues)
+    .enter()
+    .append("g")
+    .attr("class", "legend")
+    .attr("transform", (d, i) => "translate(0," + i * 20 + ")");
+
+  legend.append("rect")
+    .attr("x", width - 18)
+    .attr("width", 18)
+    .attr("height", 18)
+    .style("fill", colorScale);
+
+  legend.append("text")
+    .attr("x", width - 24)
+    .attr("y", 9)
+    .attr("dy", ".35em")
+    .style("text-anchor", "end")
+    .text(d => d);
+}
